2071727d9c03c5c45d2a1372853472fd9c81da71
theguiguix <guillaumetricaud@gmail.com>
2017-03-13 14:24:17 +0100

    Ajout du bot de base à la partie

--- Projet_JAVA/src/clientdesarenes/JeuSoloLocal.java
+++ Projet_JAVA/src/clientdesarenes/JeuSoloLocal.java
@@ -15,7 +15,7 @@ public class JeuSoloLocal {
         
         jeu.metJoueurEnPosition(0, new IA1("Carapuce", "cli5_PASS5"));
         jeu.metJoueurEnPosition(3, new Bot_GT("Bulbizarre", "cli4_PASS4"));
-        //jeu.metJoueurEnPosition(1, new jeu.Joueur("Rouge"));
+        jeu.metJoueurEnPosition(1, new Bot("Salameche", "cli6_PASS6"));
         //jeu.metJoueurEnPosition(2, new jeu.Joueur("Jaune"));
        
         FenetreDeJeu f = new FenetreDeJeu(jeu, true);
