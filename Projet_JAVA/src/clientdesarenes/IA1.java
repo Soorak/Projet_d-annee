8f217ee0773eeaa1980b6eb87552eb7499016bc7
Soorak <geof.vigneau@orange.fr>
2017-03-13 14:13:09 +0100

    BotV1
    search book and under 50 energy search to sleep

--- Projet_JAVA/src/clientdesarenes/IA1.java
+++ Projet_JAVA/src/clientdesarenes/IA1.java
@@ -21,19 +21,15 @@ public class IA1 extends jeu.Joueur implements reseau.JoueurReseauInterface {
     public Joueur.Action faitUneAction(Plateau t) {           
         Action a = null;
         
-        /*int nbrEsprit = this.donneEsprit();
+        int nbrEsprit = this.donneEsprit();
         if(nbrEsprit > 80) {
-        	
+        	a = direction(livreLePlusProche(t).get(0));
         } else if(nbrEsprit > 50) {
-        	
+        	a = direction(livreLePlusProche(t).get(0));
         }
-        else if(nbrEsprit > 30) {
-        	
-        } else {
-        	
-        }*/
-        
-        a = direction(livreLePlusProche(t));
+        else {
+        	a = direction(litLePlusProche(t).get(0));
+        }        
         
         System.out.println("Bot.faitUneAction: Je joue " + a); 
         return a;
@@ -64,14 +60,13 @@ public class IA1 extends jeu.Joueur implements reseau.JoueurReseauInterface {
         System.out.println("Bot: On est déconnecté du serveur.");
     }
     
-    public Node livreLePlusProche(Plateau t){
+    public ArrayList<Node> livreLePlusProche(Plateau t){
     	HashMap listeLivre;
     	
     	Point positionLivre;
     	
     	for(int i = 1;;++i){
     		listeLivre = t.cherche(this.donnePosition(), i, t.CHERCHE_LIVRE);
-    		
     		if (!listeLivre.isEmpty()) {
 				ArrayList<Point> arrayPointLivres = (ArrayList<Point>) listeLivre.get(2);
     			for (Point p : arrayPointLivres){
@@ -79,30 +74,47 @@ public class IA1 extends jeu.Joueur implements reseau.JoueurReseauInterface {
     	     		if(t.contientUnLivreQuiNeLuiAppartientPas(this, t.donneContenuCellule(p))){
     	     			positionLivre = p;
     	     	     	ArrayList<Node> arrayPointChemin = t.donneCheminEntre(this.donnePosition(), positionLivre);
-    	     			return arrayPointChemin.get(0);
+    	     			return arrayPointChemin;
     	     		}
     	     	}
     		}
     	}
     }
+    public ArrayList<Node> litLePlusProche(Plateau t){
+    	HashMap listeLit;
+    	
+    	Point positionLit;
+    	
+    	for(int i = 1;;++i){
+    		listeLit = t.cherche(this.donnePosition(), i, t.CHERCHE_LIT);
+    		
+    		if (!listeLit.isEmpty()) {
+				ArrayList<Point> arrayPointLit = (ArrayList<Point>) listeLit.get(1);
+    			for (Point p : arrayPointLit){
+    				positionLit = p;
+	     	     	ArrayList<Node> arrayPointChemin = t.donneCheminEntre(this.donnePosition(), positionLit);
+	     			return arrayPointChemin;
+    	     	}
+    		}
+    	}
+    }
     
-    public Node joueurLePlusProche(Plateau t){
+    public ArrayList<Node> joueurLePlusProche(Plateau t){
     	HashMap listeJoueur;
     	
     	Point positionJoueur;
     	
     	for(int i = 1;;++i){
     		listeJoueur = t.cherche(this.donnePosition(), i, t.CHERCHE_JOUEUR);
-    		
-    		if (!listeLivre.isEmpty()) {
-				ArrayList<Point> arrayPointLivres = (ArrayList<Point>) listeLivre.get(2);
-    			for (Point p : arrayPointLivres){
-    				
-    	     		if(t.contientUnLivreQuiNeLuiAppartientPas(this, t.donneContenuCellule(p))){
-    	     			positionLivre = p;
-    	     	     	ArrayList<Node> arrayPointChemin = t.donneCheminEntre(this.donnePosition(), positionLivre);
-    	     			return arrayPointChemin.get(0);
-    	     		}
+    		if (!listeJoueur.isEmpty()) {
+    			
+				ArrayList<Point> arrayPointJoueur = (ArrayList<Point>) listeJoueur.get(4);
+    			for (Point p : arrayPointJoueur){
+					positionJoueur = p;
+					if(!positionJoueur.equals(this.donnePosition())){
+						ArrayList<Node> arrayPointChemin = t.donneCheminEntre(this.donnePosition(), positionJoueur);
+		     			return arrayPointChemin;
+					}
     	     	}
     		}
     	}
