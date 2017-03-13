20e1359c0ecc7d2dbcd4db4461c4ea0b5837c6b6
theguiguix <guillaumetricaud@gmail.com>
2017-03-13 15:06:38 +0100

    Mise à jour du bot GT

--- Projet_JAVA/src/clientdesarenes/Bot_GT.java
+++ Projet_JAVA/src/clientdesarenes/Bot_GT.java
@@ -3,7 +3,10 @@ package clientdesarenes;
 import java.awt.Point;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
+import Routines.GoLit;
+import Routines.Routine;
 import jeu.Joueur;
 import jeu.Plateau;
 import jeu.Joueur.Action;
@@ -21,8 +24,10 @@ public class Bot_GT extends jeu.Joueur implements reseau.JoueurReseauInterface {
     @Override
     public Joueur.Action faitUneAction(Plateau t) {
     	Action a = super.faitUneAction(t);
-    	if(donneEsprit() < 50) {
-    		a = direction(litLePlusProche(t));
+    	if(donneEsprit() < DistanceLitPlusProche(t) + 20) {
+    		direction(litLePlusProche(t));
+    	} else {
+    		
     	}
         System.out.println("Bot.faitUneAction: Je joue " + a);
         return a;
@@ -53,7 +58,32 @@ public class Bot_GT extends jeu.Joueur implements reseau.JoueurReseauInterface {
         System.out.println("Bot: On est déconnecté du serveur.");
     }
     
-    public Action direction(Node node){
+    public int DistanceLitPlusProche(Plateau t){
+    	ArrayList<Point> lits;
+		int taille_recherche = 1;
+		do {
+			lits = t.cherche(this.donnePosition(), taille_recherche++, Plateau.CHERCHE_LIT).get(1);
+		} while (lits == null || lits.isEmpty());
+		System.err.println("J'ai " + donneEsprit() + "points d'esprit, je vais au lit !");
+		return t.donneCheminEntre(this.donnePosition(), lits.get(0)).size();
+    }
+    
+    /**
+	 * 
+	 * @param t Le plateau de jeu
+	 * @return
+	 */
+    public Node litLePlusProche(Plateau t){
+    	ArrayList<Point> lits;
+		int taille_recherche = 1;
+		do {
+			lits = t.cherche(this.donnePosition(), taille_recherche++, Plateau.CHERCHE_LIT).get(1);
+		} while (lits == null || lits.isEmpty());
+		System.err.println("J'ai " + donneEsprit() + "points d'esprit, je vais au lit !");
+		return t.donneCheminEntre(this.donnePosition(), lits.get(0)).get(0);
+    }
+	
+	public Action direction(Node node){
     	if (node.getPosX() < this.donnePosition().getX() && node.getPosY() == this.donnePosition().getY()){
     		return Action.GAUCHE;
     	} else if (node.getPosX() > this.donnePosition().getX() && node.getPosY() == this.donnePosition().getY()){
@@ -64,15 +94,5 @@ public class Bot_GT extends jeu.Joueur implements reseau.JoueurReseauInterface {
     		return Action.HAUT;
     	}
     	return null;
-    }
-    
-    public Node litLePlusProche(Plateau t){
-    	ArrayList<Point> lits;
-		int taille_recherche = 1;
-		do {
-			lits = t.cherche(this.donnePosition(), taille_recherche++, Plateau.CHERCHE_LIT).get(1);
-		} while (lits == null || lits.isEmpty());
-		System.err.println("J'ai " + donneEsprit() + "points d'esprit, je vais au lit !");
-		return t.donneCheminEntre(this.donnePosition(), lits.get(0)).get(0);
     }
 }
