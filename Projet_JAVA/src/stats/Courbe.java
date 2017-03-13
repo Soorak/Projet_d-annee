fe1f7864641d225cf1bc277922029257da0b0a7c
theguiguix <guillaumetricaud@gmail.com>
2017-03-13 23:08:42 +0100

    Ajout d'un dénominateur
    L'ajout du dénominateur evite les courbes trop petites

--- Projet_JAVA/src/stats/Courbe.java
+++ Projet_JAVA/src/stats/Courbe.java
@@ -13,6 +13,14 @@ import java.nio.charset.Charset;
 
 public class Courbe extends Applet {
 	
+	private Label l;
+	
+	public void init () {
+		l = new Label();
+		l.setBackground(new Color(132,179,255));
+		this.add(l);
+	}
+	
 	public void paint(Graphics g) {
 		File dossier = new File("../stats/");
 		File[] fichiers = dossier.listFiles();
@@ -47,20 +55,31 @@ public class Courbe extends Applet {
 		
 		int plusGrandeValeur = 0;
 		for(int i = 0; i < avgCult.length; i++) {
-			avgCult[i] = sumCult[i] / values.length;
+			avgCult[i] = sumCult[i] / fichiers.length;
 			if(avgCult[i] > plusGrandeValeur) plusGrandeValeur = avgCult[i];
+		}
+		
+		int denominateur = 2;
+		if(plusGrandeValeur > 800 && plusGrandeValeur < 1600) {
+			denominateur = 3;
+		} else if (plusGrandeValeur > 1600 && plusGrandeValeur < 2400) {
+			denominateur = 4;
+		} else if (plusGrandeValeur > 2400 && plusGrandeValeur < 3200) {
+			denominateur = 5;
+		} else if (plusGrandeValeur > 3200 && plusGrandeValeur < 4000) {
+			denominateur = 6;
+		} else if (plusGrandeValeur > 4000){
+			denominateur = 7;
 		}
 		
 		for(int i = 0; i < avgCult.length; i++) {
 			avgCult[i] = sumCult[i] / fichiers.length;
 			if(i != 0) {
-				g.drawLine(avgCult[i-1]/5,(300-(i-1)),avgCult[i]/5,(300-i));
+				g.drawLine(avgCult[i-1]/denominateur,(300-(i-1)),avgCult[i]/denominateur,(300-i));
 			}
 		}
 
-		this.setSize(plusGrandeValeur/5 + 20, 320);
-		Label l = new Label("Tour 300 : " + plusGrandeValeur + " pts de culture (en moyenne)");
-		l.setBackground(new Color(132,179,255));
-		this.add(l);
+		this.setSize(plusGrandeValeur/denominateur + 20, 320);
+		l.setText("Tour 300 : " + plusGrandeValeur + " pts de culture (en moyenne)");
 	}
 }
