d65edf6ac44f77e9f66a74dc763d22e93750e6a6
Soorak <geof.vigneau@orange.fr>
2017-03-13 11:17:44 +0100

    test IA

--- Projet_JAVA/src/clientdesarenes/IA1.java
+++ Projet_JAVA/src/clientdesarenes/IA1.java
@@ -1,5 +1,8 @@
 package clientdesarenes;
 
+import java.awt.Point;
+import java.util.HashMap;
+
 import jeu.Joueur;
 import jeu.Plateau;
 import jeu.Joueur.Action;
@@ -15,6 +18,35 @@ public class IA1 extends jeu.Joueur implements reseau.JoueurReseauInterface {
     @Override
     public Joueur.Action faitUneAction(Plateau t) {   
         Action a = super.faitUneAction(t);
+        
+        
+        Point positionJoueur = this.donnePosition();
+        
+        Point destinationDroite = new Point((int) positionJoueur.getX() + 1, (int) positionJoueur.getY());
+        Point destinationGauche = new Point((int) positionJoueur.getX() - 1, (int) positionJoueur.getY());
+        Point destinationHaut = new Point((int) positionJoueur.getX(), (int) positionJoueur.getY() - 1);
+        Point destinationBas = new Point((int) positionJoueur.getX(), (int) positionJoueur.getY() + 1);
+        
+        HashMap listeLivre = t.cherche(positionJoueur, 3, t.CHERCHE_LIVRE);
+        if(!listeLivre.isEmpty()){
+        	t.
+        }
+        Action.
+        
+        
+        int nbrEsprit = this.donneEsprit();
+        if(nbrEsprit > 80) {
+        	
+        } else if(nbrEsprit > 50) {
+        	
+        }
+        else if(nbrEsprit > 30) {
+        	
+        } else {
+        	
+        }
+        
+        
         System.out.println("Bot.faitUneAction: Je joue " + a); 
         return a;
     }
@@ -43,4 +75,23 @@ public class IA1 extends jeu.Joueur implements reseau.JoueurReseauInterface {
     public void deconnecte() {
         System.out.println("Bot: On est déconnecté du serveur.");
     }
+    
+    
+    boolean estUnObstacle (int position, Plateau t){
+    	
+		return false;
+    }
+    boolean estUnSpawn (int position, Plateau t){
+		return false;
+    }
+    boolean estUnLivre (int position, Plateau t){
+		return false;
+    }
+    boolean estUnLit (int position, Plateau t){
+		return false;
+    }
+    boolean estUnPersonnage (int position, Plateau t){
+		return false;
+    }
+
 }
--- Projet_JAVA/src/clientdesarenes/JeuSoloLocal.java
+++ Projet_JAVA/src/clientdesarenes/JeuSoloLocal.java
@@ -13,8 +13,8 @@ public class JeuSoloLocal {
         
         MaitreDuJeu jeu = new MaitreDuJeu(p);
         
-        jeu.metJoueurEnPosition(0, new Bot("Carapuce", "cli5_PASS5"));
-        jeu.metJoueurEnPosition(3, new Bot("Bulbizarre", "cli4_PASS4"));
+        jeu.metJoueurEnPosition(0, new IA1("Carapuce", "cli5_PASS5"));
+        //jeu.metJoueurEnPosition(3, new Bot("Bulbizarre", "cli4_PASS4"));
         //jeu.metJoueurEnPosition(1, new jeu.Joueur("Rouge"));
         //jeu.metJoueurEnPosition(2, new jeu.Joueur("Jaune"));
        
